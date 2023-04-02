240c071ad7b4b7a315f25ac3cf233da5a9565f37
sopesa <og.soos.peter@gmail.com>
2023-04-02 18:59:41 +0200

    can send GET request for temp data & plot the data on a nice chart

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,16 @@
+import 'dart:ffi';
+
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:http/http.dart';
+import 'package:syncfusion_flutter_charts/charts.dart';
+
+class TemperatureData {
+  final int sequence;
+  final num data;
+
+  TemperatureData(this.sequence, this.data);
+}
 
 void main() {
   runApp(const MyApp());
@@ -7,39 +19,22 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+  final Client client = http.Client();
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
+  MyHomePage({super.key, required this.title});
 
   final String title;
 
@@ -48,59 +43,51 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  List<TemperatureData> tempData = [];
+  void _incrementCounter() async {
+    // allow = !allow;
 
-  void _incrementCounter() {
+    var response = await widget.client.get(Uri.parse('http://192.168.0.192:80'));
+    print('Response status: ${response.statusCode}');
+    print('Response body: ${response.body}');
+
+    List<String> tempLogs = response.body.trim().split('\n');
+    int index = 0;
+    List<TemperatureData> tmpTempData = [];
+
+    tempLogs.forEach((tempLog) {
+      tmpTempData.add(TemperatureData(index, double.parse(tempLog)));
+      index++;
+    });
+
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      tempData = tmpTempData;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+        child: SfCartesianChart(
+          primaryXAxis: NumericAxis(),
+          primaryYAxis: NumericAxis(
+            title: AxisTitle(
+              text: '°C',
+              alignment: ChartAlignment.far,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            maximum: 35,
+            minimum: 10,
+          ),
+          series: <ChartSeries>[
+            // Renders line chart
+            LineSeries<TemperatureData, int>(
+              dataSource: tempData,
+              xValueMapper: (TemperatureData tempLog, _) => tempLog.sequence,
+              yValueMapper: (TemperatureData tempLog, _) => tempLog.data,
             ),
           ],
         ),
@@ -108,8 +95,8 @@ class _MyHomePageState extends State<MyHomePage> {
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        child: const Icon(Icons.thermostat),
+      ),
     );
   }
 }
